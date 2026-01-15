3e36031a0a97dc9945bcdaf1dd83a674c9df3c91
sdram58 <sdram58@hotmail.com>
2026-01-15 19:36:27 +0100

    SplashScreen_Updated

--- UD8/SplashScreen/app/src/main/java/com/catata/splashscreen/MainActivity.kt
+++ UD8/SplashScreen/app/src/main/java/com/catata/splashscreen/MainActivity.kt
@@ -11,37 +11,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.catata.splashscreen.navigation.Navigation
 import com.catata.splashscreen.ui.theme.SplashScreenTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //In newer versions we don't need install SplashScreen
+
+        /*val splashScreen = installSplashScreen()
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            splashScreenView.remove()
+            // Se vuelve a asignar el tema ya que al eliminar la Splash Screen se elimina el tema
+            setTheme(R.style.Theme_SplashScreen)
+            // Volver a crear la actividad para aplicar el nuevo tema
+            recreate()
+        }
+        splashScreen.setKeepOnScreenCondition { false }*/
         enableEdgeToEdge()
         setContent {
             SplashScreenTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Navigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SplashScreenTheme {
-        Greeting("Android")
     }
 }
