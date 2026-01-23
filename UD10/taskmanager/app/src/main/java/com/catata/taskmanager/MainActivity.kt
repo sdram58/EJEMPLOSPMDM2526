6f382f0e6e25cc0fed8e1cd741eef6c03012235f
sdram58 <sdram58@hotmail.com>
2026-01-23 12:26:24 +0100

    UD10_samples_uploaded

---
+++ UD10/taskmanager/app/src/main/java/com/catata/taskmanager/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.catata.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+
+import com.catata.taskmanager.navigation.Navigation
+import com.catata.taskmanager.tasks.ui.viewmodel.TaskViewModel
+import com.catata.taskmanager.ui.theme.TaskManagerTheme
+
+import kotlin.getValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaskManagerTheme {
+                val taskViewModel by viewModels<TaskViewModel>()
+                Navigation(taskViewModel)
+            }
+        }
+    }
+}
